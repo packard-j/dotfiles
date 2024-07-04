60618ba2ffcd0b42f31cc6e935829d7b0d4b6a44
James Packard <40209595+packard-j@users.noreply.github.com>
2024-07-04 14:16:39 -0400

    add jsdoc plugin for neovim

--- nvim/init.lua
+++ nvim/init.lua
@@ -88,7 +88,7 @@ require('evil_lualine')
 
 require('nvim-treesitter.configs').setup({
   -- A list of parser names, or "all" (the five listed parsers should always be installed)
-  ensure_installed = { "haskell", "typescript", "lua", "html" },
+  ensure_installed = { "haskell", "typescript", "lua", "html", "jsdoc" },
 
   -- Install parsers synchronously (only applied to `ensure_installed`)
   sync_install = false,
