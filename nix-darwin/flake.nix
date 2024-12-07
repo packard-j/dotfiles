4f2efdbfb51604e351ff300ca3d786d77d55a26e
James Packard <40209595+packard-j@users.noreply.github.com>
2024-12-06 20:13:55 -0500

    upgrade to nixos 24.11

--- nix-darwin/flake.nix
+++ nix-darwin/flake.nix
@@ -2,8 +2,8 @@
   description = "James' Darwin system flake";
 
   inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.05-darwin";
-    home-manager.url = "github:nix-community/home-manager/release-24.05";
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-24.11-darwin";
+    home-manager.url = "github:nix-community/home-manager/release-24.11";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
     darwin.url = "github:LnL7/nix-darwin";
     darwin.inputs.nixpkgs.follows = "nixpkgs";
